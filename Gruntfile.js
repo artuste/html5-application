21d3b38700af59450bde776260183940db0f3a5e
art <artuste@gmail.com>
2014-08-18 23:45:01 +0200

    Clean up.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,6 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        jasmine: {
-            assertions: {
-                src: 'app/tests/jasmineSpec.js',
-                options: {
-                    specs: 'app/tests/jasmineSpec.js'
-                }
-            }
-        },
-        karma: {
-            continuous: {
-                configFile: 'karma.conf.js',
-                singleRun: true,
-                browsers: ['PhantomJS'] //'Chrome', 'Firefox'
-            }
-        },
         wiredep: {
             target: {
                 src: [
@@ -79,37 +64,21 @@ module.exports = function (grunt) {
                 tasks: ['jshint']
             },
             html: {
-                files: ['app/modules/*/*/tpl/*.html', 'app/modules/*/*/*/tpl/*.html'],
+                files: ['app/tpl/*.html'],
                 tasks: ['htmlhint']
-            },
-            jasmine: {
-                files: ['app/tests/jasmineSpec.js'],
-                tasks: 'jasmine'
-            },
-            karma: {
-                files: ['app/tests/testSpec.js'],
-                tasks: 'karma'
             }
         }
     });
 
-    // Npm tasks
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-
-    grunt.loadNpmTasks('grunt-contrib-jasmine');
-    grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-wiredep');
-
-    //    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-htmlhint');
 
-    // My tasks
     grunt.registerTask('hints', ['jshint', 'htmlhint']);
     grunt.registerTask('less-css', ['less', 'cssmin']);
-
     grunt.registerTask('default', ['less-css', 'wiredep']);
 };
