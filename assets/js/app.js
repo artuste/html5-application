a1a98475f3e15db25d3a913cb710a2d9b36e441a
art <artuste@gmail.com>
2014-08-22 01:15:45 +0200

    .

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,10 @@
 $(function () {
+var $parent = $('.navbar-search-filter');
 
+    $('.search-button', $parent).click(function () {
+        $parent.hide();
+        $parent.next().removeClass('hidden');
+
+        return false;
+    });
 });
