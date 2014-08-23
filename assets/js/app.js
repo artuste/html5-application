674ec3db8810a0ded243fefff393e9b6a0c6882d
art <artuste@gmail.com>
2014-08-23 13:23:02 +0200

    .

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,52 @@
 $(function () {
-var $parent = $('.navbar-search-filter'),
-    $boxWrapper = $(".box-wrapper"),
-    $panelActions = $('.panel-actions'),
-    $panelTop = $('.panel-top');
 
-    $('.search-button', $parent).click(function () {
+    var $parent = $('.navbar-search-filter'),
+        $boxWrapper = $(".box-wrapper"),
+        $panelActions = $('.panel-actions'),
+        $panelTop = $('.panel-top'),
+        $searchBtn = $('.search-button', $parent),
+        $arrowBtn = $('.arrow');
+
+    $searchBtn.click(function () {
         $parent.hide();
-        $parent.next().removeClass('hidden');
+        $parent.next().fadeIn(300).removeClass('hidden');
 
         return false;
     });
-    
-    function initScrollbar() {
+
+    $arrowBtn.click(function () {
+        var $this = $(this, $parent),
+            isBoxWrapper = $this.parents('.box-toggled-wrapper').is(':visible');
+
+        if (isBoxWrapper) {
+            if ($this.hasClass('revert')) {
+                $this.parent().next().fadeOut(200);
+                $this.removeClass('revert');
+            } else {
+                $this.parent().next().fadeIn(200);
+                $this.addClass('revert');
+            }
+        }
+
+        return false;
+    });
+
+    function initScrollbar () {
         var globalHeight = window.innerHeight,
-        panelActionsBottomHeight = $panelActions.height(),
-        panelTopHeight = $panelTop.height(),
-        boxWrapperHeight = (globalHeight - panelActionsBottomHeight - panelTopHeight);
-        
+            panelActionsBottomHeight = $panelActions.height(),
+            panelTopHeight = $panelTop.height(),
+            boxWrapperHeight = (globalHeight - panelActionsBottomHeight - panelTopHeight);
+
         $boxWrapper
             .css('height', boxWrapperHeight)
             .mCustomScrollbar({
                 theme: 'dark'
             });
     }
-    
-    window.onresize = function(event) {
-       initScrollbar();
+
+    window.onresize = function (event) {
+        initScrollbar();
     };
-    
+
     initScrollbar();
 });
